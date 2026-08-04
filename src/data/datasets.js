a181ef5e79ee26c26139ca2bdcb670fef8dfc836
Xinhua Li <118495925+Larrtroffen@users.noreply.github.com>
2026-08-04 21:02:42 +0800

    eomain: update src/data/datasets.js

--- src/data/datasets.js
+++ src/data/datasets.js
@@ -34,6 +34,7 @@ export const DATASET_CATEGORIES = [
     i18nKey: 'products.categories.code',
     artifact: 'CodeArtifact',
     datasets: [
+      'auto-test',
       'oj-competition',
       'scicode',
       'swe-atlas-qna',
